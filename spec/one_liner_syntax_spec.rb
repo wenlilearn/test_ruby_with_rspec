cf5b6624a13694cab68135156f0bc06012563806
Wen Li <wli@terakeet.com>
2020-01-30 13:00:56 -0500

    Oneliner

---
+++ spec/one_liner_syntax_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe 'shorthand syntax' do
+  subject { 5 }
+
+  context 'with classic syntax' do
+    it 'should equal 5' do
+      expect(subject).to eq(5)
+    end
+  end
+
+  context 'with one-liner' do
+    it { is_expected.to eq(5) }
+    it { expect(subject).to eq(5) }
+  end
+end
