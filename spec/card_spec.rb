01833e0e34e304a6e83f9300e4fc0ba813e3fa81
wenlilearn <wenlilearn@gmail.com>
2020-01-30 20:33:51 -0500

    card spec

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -9,15 +9,18 @@ end
 
 
 RSpec.describe Card do
-  before do
-    @card = Card.new('Ace', 'Spade')
-  end
+  let(:card) { Card.new('Ace', 'Spades') }
 
   it "has a rank" do
-    expect(@card.rank).to eq('Ace')
+    expect(card.rank).to eq('Ace')
   end
 
   it "has a suit" do
-    expect(@card.suit).to eq('Spade')
+    expect(card.suit).to eq('Spades')
+  end
+
+  it "has a custom error message" do
+    comparison = 'Spade'
+    expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} but I got #{card.suit} instead!"
   end
 end
